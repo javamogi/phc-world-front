002ac97979b7c326422d32b0d1cc02a3af30a55a
javamogi <40781237+javamogi@users.noreply.github.com>
2023-12-14 20:24:37 +0900

    feat: 로그인 추가

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,25 +7,19 @@
                         <h3 class="panel-title">Please Sign In</h3>
                     </div>
                     <div class="panel-body">
-                    errorMessage
-                    	<div class="alert alert-danger alert-dismissable">
-                        	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                             errorMessage
-                         </div>
-                    errorMessage
-                    authMessage
-                    	<div class="alert alert-success alert-dismissable">
-                        	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                             authMessage
-                         </div>
-                    authMessage
-                        <form role="form" method="post" action="/users/login">
+                        <div v-show="show" >
+                            <div class="alert alert-danger alert-dismissable">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                {{ errorMessage }}
+                            </div>
+                        </div>
+                        <form v-on:submit.prevent="submit" role="form" method="post" action="">
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" id="email" name="email" type="email" autofocus>
+                                    <input @keyup="regexEmail" class="form-control" placeholder="E-mail" id="email" name="email" type="email" v-model="email" autofocus>
                                 </div>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                    <input @keyup="regexPassword" class="form-control" placeholder="Password" name="password" type="password" v-model="password">
                                 </div>
                                 <div class="checkbox">
                                     <label>
@@ -52,8 +46,77 @@
 </template>
 
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+    data(){
+        return {
+            email: '',
+            password: '',
+            statusCode: '',
+            errorMessage: '',
+            show: false
+        }
+    },
+    methods: {
+        regexEmail(){
+            const regex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+            let email = this.email;
+            if(email.length < 3){
+                this.errorMessage = "이메일은 3자 이상으로 해야합니다.";
+                this.show = true;
+            } else if(!regex.test(email)){
+                this.errorMessage = "이메일 형식이 아닙니다";
+                this.show = true;
+            } else {
+                this.show = false;
+            }
+        },
+        regexPassword(){
+            let password = this.password;
+            if(password.length < 4){
+                this.errorMessage = "비밀번호는 4자 이상으로 해야합니다.";
+                this.show = true;
+            } else {
+                this.show = false;
+            }
+        },
+        submit(){
+            this.regexEmail();
+            this.regexPassword();
+            if(this.show){
+                return;
+            }
+           
+            const apiUrl = this.$store.state.apiUrl;
+
+            this.statusCode = '';
+            const userData = new FormData();
+            userData.append("email", this.email);
+            userData.append("password", this.password);
+
+            axios
+            .post(apiUrl + '/api/users/login', userData, {})
+            .then((res) => {
+                if(res.status === 200){
+                    console.log(res);
+                    // TO DO 토큰 처리
+                    // location.href='/';
+                }
+            })
+            .catch((res) => {
+                // console.error(res);
+                let statusCode = res.response.status;
+                this.statusCode = statusCode;
+                this.show = true;
+                if(statusCode === 400){                    
+                    this.errorMessage = "비밀번호가 틀렸습니다."
+                } else {
+                    this.errorMessage = "존재하지 않는 아이디입니다.";
+                }
+            })
+        }
+    }
 }
 </script>
 
